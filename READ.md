d297c63984f3fbc48ade6df592357b7f74070816
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-10-08 12:44:18 -0400

    Init commit

---
+++ READ.md
@@ -0,0 +1,23 @@
+This Project takes in a list of gardens.
+4
+2 3
+3 4
+1 3
+2 2
+
+
+
+The first Number is the length of the array 
+The next numbers are the distance of the gardens from 0 and the second numbers are the garden's size 
+
+The cost is (distance) x (size)
+
+The aim of the program is to find the least cost to build a garden by 
+placing the base x = middle of the distances.
+
+
+Implements 
+    -QuickSort
+    -Recusion
+    -Objects
+    
---
+++ src/cmsc401.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class cmsc401 {
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+//        Garden Array
+
+        int sizeArray = in.nextInt();
+        garden [] gardenArray = new garden[sizeArray];
+        int i = 0;
+        while(i < sizeArray){
+            int gardenDistance = in.nextInt();
+            int gardenSize = in.nextInt();
+            garden garden = new garden(gardenDistance, gardenSize);
+            gardenArray[i] = garden;
+            i++;
+        }
+        sortArray(gardenArray, 0, gardenArray.length - 1);
+        printArr(gardenArray);
+        System.out.println(returnMid(gardenArray));
+    }
+//    To swap the elements
+    public static void swap(garden[] array, int a, int b){
+        garden dummy = array[a];
+        array[a] = array[b];
+        array[b] = dummy;
+
+
+    }
+//    For quality we must print the arr
+    public static void printArr(garden[] array){
+        for(garden x: array){
+            System.out.println(x.getDistance() + " " + x.getSize());
+        }
+    }
+
+    public static int partition(garden[] array, int begining, int end){
+        garden dummy = array[end];
+        int i = begining - 1;
+        for(int j = begining; j <= end-1; j++){
+            if(array[j].getDistance() < dummy.getDistance()){
+
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i+1, end);
+
+        return i+1;
+    }
+    public static void sortArray(garden[] array, int beginning, int end){
+       if(beginning < end) {
+           int partition = partition(array, beginning, end);
+           sortArray(array, beginning, partition - 1);
+           sortArray(array, partition + 1, end);
+
+       }
+    }
+// Returns the mid with the highest garden size eaither mid, mid + 1, or mid - 1
+    public static int returnMid(garden[] array){
+        if(array.length == 0)
+            return 0;
+        int mid = array.length / 2;
+        if(array[mid].getSize() >= array[mid-1].getSize() && array[mid].getSize() >= array[mid+1].getSize())
+            return array[mid].getDistance();
+        if(array[mid + 1].getSize() >= array[mid].getSize() && array[mid + 1].getSize() >= array[mid - 1].getSize())
+            return array[mid+1].getDistance();
+        if(array[mid - 1 ].getSize() >= array[mid].getSize() && array[mid - 1].getSize() >= array[mid+1].getSize())
+            return array[mid - 1].getDistance();
+        return 0;
+    }
+
+
+//10 + 4 + 2 + 0 + 18 = 34  CORRECT!!!!!
+//6 + 2 + 0 + 4 + 24 = 36
+
+//14 + 15 + 8 + 0 + 1 = 38 CORRECT!!!!
+//6 + 3 + 0 + 20 + 10 = 39
+}
+
+
+
+
+
+
+
+//Created this object to hold the both data aspects of the garden
+class garden{
+    private int distance;
+    private int size;
+    public garden(int aDistance, int aSize){
+        distance = aDistance;
+        size = aSize;
+    }
+    public int getDistance(){
+        return distance;
+    }
+    public int getSize(){
+        return size;
+    }
+}
